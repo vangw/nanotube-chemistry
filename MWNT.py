0dd5c5a555d56bd424036757b3b874807dff9300
vangw <143125074+vangw@users.noreply.github.com>
2023-11-09 17:37:13 +0000

    Update MWNT.py

--- MWNT.py
+++ MWNT.py
@@ -11,8 +11,8 @@ import sys
 import os
 
 # In the command line input the size of the nanotubes require as "n1_m1 n2_m2"
-innertube = "/home/mw3/mw/spet5177/SWNT_xyz/" + str(sys.argv[1]) + ".xyz"
-outertube = "/home/mw3/mw/spet5177/SWNT_xyz/" + str(sys.argv[2]) + ".xyz"
+innertube = "<path_to_folder>" + str(sys.argv[1]) + ".xyz"
+outertube = "<path_to_folder>" + str(sys.argv[2]) + ".xyz"
 print(innertube, outertube)
 
 # Function to load XYZ data from a file
@@ -36,18 +36,10 @@ offset = np.array([0, 0, 0]) # Adjust this offset as needed
 # Combine the XYZ coordinates
 combined_xyz = np.vstack((nanotube1_xyz, nanotube2_xyz + offset))
 output_file_name = "(" + str(sys.argv[1]) + ")" + "(" + str(sys.argv[2]) + ").xyz"
-output_file_path = os.path.join("/home/mw3/mw/spet5177/MWNT_xyz/", output_file_name)
+output_file_path = os.path.join("<path_to_folder>", output_file_name)
 print(output_file_path)
 # Write the combined XYZ coordinates to a new XYZ file
 with open(output_file_path, "w") as output_file:
     output_file.write("{}\n\n".format(combined_xyz.shape[0]))
     for i in range(combined_xyz.shape[0]):
-        output_file.write("C{} {:<10} {:<10} {:<10}\n".format(i + 1, combined_xyz[i, 0], combined_xyz[i, 1], combined_xyz[i, 2]))
-
-
-
-# with open("nanotube_{:}_{:}.xyz".format(n, m), "w") as vmd:
-#    vmd.write("{:}\n\n " .format(tube.shape[0]))
-#    for i in range(tube.shape[0]):
-#        vmd.write("C{:} {:<10} {:<10} {:<10}\n" .format(i+1, tube[i,0], tube[i,1], tube[i,2]))
-        
+        output_file.write("C{} {:<10} {:<10} {:<10}\n".format(i + 1, combined_xyz[i, 0], combined_xyz[i, 1], combined_xyz[i, 2]))        
